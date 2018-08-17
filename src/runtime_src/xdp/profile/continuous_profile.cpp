5ae1f627762575c86312b0954cfeeec1ccaf17d8
Tianhao Zhou <tianhaoz@umich.edu>
2018-08-17 14:18:45 -0700

    power trace should be working now

--- src/runtime_src/xdp/profile/continuous_profile.cpp
+++ src/runtime_src/xdp/profile/continuous_profile.cpp
@@ -79,7 +79,28 @@ void PowerMonitor::willLaunch() {
 	power_dump_file.open(dump_filename, std::ofstream::out | std::ofstream::trunc);
 	power_dump_file.close();
 	power_dump_file.open(dump_filename, std::ofstream::app);
-	power_dump_file << "Timestamp,FPGA Power Consumption,Board Power Consumption" << std::endl;
+	power_dump_file << "timestamp" << ",";
+	power_dump_file << "mTimeStamp" << ",";
+	power_dump_file << "mVInt" << ",";
+	power_dump_file << "mVAux" << ",";
+	power_dump_file << "mVBram" << ",";
+	power_dump_file << "m12VPex" << ",";
+	power_dump_file << "m12VAux" << ",";
+	power_dump_file << "mPexCurr" << ",";
+	power_dump_file << "mAuxCurr" << ",";
+	power_dump_file << "m3v3Pex" << ",";
+	power_dump_file << "m3v3Aux" << ",";
+	power_dump_file << "mDDRVppBottom" << ",";
+	power_dump_file << "mDDRVppTop" << ",";
+	power_dump_file << "mSys5v5" << ",";
+	power_dump_file << "m1v2Top" << ",";
+	power_dump_file << "m1v8Top" << ",";
+	power_dump_file << "m0v85" << ",";
+	power_dump_file << "mMgt0v9" << ",";
+	power_dump_file << "m12vSW" << ",";
+	power_dump_file << "mMgtVtt" << ",";
+	power_dump_file << "m1v2Bottom";
+	power_dump_file << std::endl;
 	power_dump_file.flush();
 }
 
@@ -91,7 +112,26 @@ XclPowerInfo PowerMonitor::readPowerStatus() {
 void PowerMonitor::outputPowerStatus(XclPowerInfo& status) {
 	auto timestamp = std::chrono::system_clock::now().time_since_epoch().count();
 	power_dump_file << timestamp << ",";
-	power_dump_file << status.m3v3Pex;
+	power_dump_file << status.mTimeStamp << ",";
+	power_dump_file << status.mVInt << ",";
+	power_dump_file << status.mVAux << ",";
+	power_dump_file << status.mVBram << ",";
+	power_dump_file << status.m12VPex << ",";
+	power_dump_file << status.m12VAux << ",";
+	power_dump_file << status.mPexCurr << ",";
+	power_dump_file << status.mAuxCurr << ",";
+	power_dump_file << status.m3v3Pex << ",";
+	power_dump_file << status.m3v3Aux << ",";
+	power_dump_file << status.mDDRVppBottom << ",";
+	power_dump_file << status.mDDRVppTop << ",";
+	power_dump_file << status.mSys5v5 << ",";
+	power_dump_file << status.m1v2Top << ",";
+	power_dump_file << status.m1v8Top << ",";
+	power_dump_file << status.m0v85 << ",";
+	power_dump_file << status.mMgt0v9 << ",";
+	power_dump_file << status.m12vSW << ",";
+	power_dump_file << status.mMgtVtt << ",";
+	power_dump_file << status.m1v2Bottom;
 	power_dump_file << std::endl;
 	power_dump_file.flush();
 }
