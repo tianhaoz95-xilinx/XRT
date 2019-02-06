8e722ce5d20b9bd0cef4126f92536fc72a2d34ad
Tianhao Zhou <tianhaoz@umich.edu>
2019-02-05 16:20:46 -0800

    test

--- src/runtime_src/xrt/scheduler/kds.cpp
+++ src/runtime_src/xrt/scheduler/kds.cpp
@@ -232,7 +232,7 @@ stop()
 void
 init(xrt::device* device, size_t regmap_size, bool cu_isr, size_t num_cus, size_t cu_offset, size_t cu_base_addr, const std::vector<uint32_t>& cu_addr_map)
 {
-  std::cout << "DEBUG: kds init start" << std:endl;
+  std::cout << "DEBUG: kds init start" << std::endl;
   auto cudma = xrt::config::get_ert_cudma();
   if (cudma && regmap_size>=0x210 && is_51_dsa(device)) {
     // bug in cudma.c HW
@@ -245,7 +245,7 @@ init(xrt::device* device, size_t regmap_size, bool cu_isr, size_t num_cus, size_
     cudma = false;
   }
 
-  std::cout << "DEBUG: messages sent to cudma" << std:endl;
+  std::cout << "DEBUG: messages sent to cudma" << std::endl;
 
   auto configure = std::make_shared<command>(device,ERT_CONFIGURE);
   auto epacket = xrt::command_cast<ert_configure_cmd*>(configure.get());
@@ -265,7 +265,7 @@ init(xrt::device* device, size_t regmap_size, bool cu_isr, size_t num_cus, size_
 
   // cu addr map
   std::copy(cu_addr_map.begin(), cu_addr_map.end(), epacket->data);
-  std::cout << "DEBUG: done cu addr mapping" << std:endl;
+  std::cout << "DEBUG: done cu addr mapping" << std::endl;
 
   // payload size
   epacket->count = 5 + cu_addr_map.size();
